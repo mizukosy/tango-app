364c31c636dab1bc204a57312008eb8dbb280540
mizukosy <kazukoshy@gmail.com>
2020-10-19 12:43:33 +0900

    ルーティング実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
-  resources :questions, only: [:index, :new, :create, :show]
+  resources :questions, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :counts, only: [:index]
 end
